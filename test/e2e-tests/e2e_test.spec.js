3295e6849862cc02f93b9089f8c3d25a8b5132e8
NoVittuBorje <jere.poysti@hotmail.com>
2026-06-29 17:38:43 +0300

    Pokemon page test

--- test/e2e-tests/e2e_test.spec.js
+++ test/e2e-tests/e2e_test.spec.js
@@ -7,3 +7,10 @@ describe('Pokedex', () => {
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
 })
+describe('Pokedex pokemon page', () =>{
+  test('front page can be opened', async ({ page }) => {
+    await page.goto('localhost:8080')
+    await page.getByText('bulbasaur').click()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+  })
+})
