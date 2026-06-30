7869159469c7940bdddad7b9c6896ddadd6f3ddd
NoVittuBorje <jere.poysti@hotmail.com>
2026-06-30 14:08:05 +0300

    fixed deployment

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ const start = async () => {
   await app.listen(PORT)
   console.log(`server started on port ${PORT}`)
   app.get('/health', (req, res) => {
-    // eslint-disable-next-line no-constant-condition
-    if (true) throw('error...  ')
     res.send('ok')
   })
 }
